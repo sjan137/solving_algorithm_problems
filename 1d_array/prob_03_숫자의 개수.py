d73ded39917cd365d88bc55d3818257d484fe5f7
Seongjin Ahn <sjan137@naver.com>
2021-07-03 11:41:03 +0900

    Update prob_03_숫자의 개수.py

--- 1d_array/prob_03_숫자의 개수.py
+++ 1d_array/prob_03_숫자의 개수.py
@@ -1,1 +1,11 @@
+import sys
 
+a = int(sys.stdin.readline())
+b = int(sys.stdin.readline())
+c = int(sys.stdin.readline())
+abc = list(str(a * b * c))
+abc.sort()
+
+for i in range(10):
+    count = abc.count(str(i))
+    print(count)
