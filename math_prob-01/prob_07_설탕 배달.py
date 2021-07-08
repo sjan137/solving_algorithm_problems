9ef8eda3719f1791b465189e908acb685a639e57
Seongjin Ahn <sjan137@naver.com>
2021-07-08 12:35:31 +0900

    Update prob_07_설탕 배달.py

--- math_prob-01/prob_07_설탕 배달.py
+++ math_prob-01/prob_07_설탕 배달.py
@@ -1,15 +1,16 @@
 N = int(input())
-a_5, b_5 = divmod(N, 5)
-if b_5 == 0:
-    print(a_5)
+count = 0
+if N % 5 == 0:
+    print(N // 5)
+elif N % 3 == 0:
+    print(N // 3)
 else:
-    if (b_5 % 3) == 0:
-        print(a_5 + (b_5 // 3))
-    elif N % 3 == 0:
-        print(N // 3)
-    else:
-        a_8, b_8 = divmod(N, 8)
-        if b_8 == 0: print(a_8 * 2)
-        elif (b_8 == 3) or (b_8 == 5): print(a_8 * 2 + 1)
-        elif b_8 == 6: print((a_8 + 1) * 2)
-        else: print(-1)
+    while True:
+        N -= 5
+        count += 1
+        if N % 3 == 0:
+            print(N // 3 + count)
+            break
+        if N < 3:
+            print(-1)
+            break
