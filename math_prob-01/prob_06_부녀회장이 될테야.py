166e4ea2b43db97b88e4c9dfad2144525a1b35f8
Seongjin Ahn <sjan137@naver.com>
2021-07-09 18:48:57 +0900

    210709_solved

---
+++ math_prob-01/prob_06_부녀회장이 될테야.py
@@ -0,0 +1,16 @@
+import sys
+
+T = int(sys.stdin.readline())
+for t in range(T):
+    K = int(sys.stdin.readline())
+    N = int(sys.stdin.readline())
+    n_list = list()
+    temp_list = [0] * N
+    for n in range(N):
+        n_list.append(n+1)
+    for k in range(1, K+1):
+        for n in range(N):
+            temp_list[n] = sum(n_list[:n+1])
+        n_list = temp_list[:]
+        print(n_list)
+    print(n_list[n])
