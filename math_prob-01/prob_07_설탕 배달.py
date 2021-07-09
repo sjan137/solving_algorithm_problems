758827c8e9acd27d024289a7e23bba79b109ac3d
Seongjin Ahn <sjan137@naver.com>
2021-07-09 18:15:57 +0900

    210709_solved

--- math_prob-01/prob_07_설탕 배달.py
+++ math_prob-01/prob_07_설탕 배달.py
@@ -1,16 +1,10 @@
 N = int(input())
-count = 0
-if N % 5 == 0:
-    print(N // 5)
-elif N % 3 == 0:
-    print(N // 3)
-else:
-    while True:
-        N -= 5
-        count += 1
-        if N % 3 == 0:
-            print(N // 3 + count)
-            break
-        if N < 3:
-            print(-1)
-            break
+count = list()
+a5 = N // 5
+for i in range(0, a5 + 1):
+    b3 = (N - 5 * i) % 3
+    if b3 == 0:
+        a3 = (N - 5 * i) // 3
+        count.append(i + a3)
+if len(count) == 0: print(-1)
+else: print(min(count))
