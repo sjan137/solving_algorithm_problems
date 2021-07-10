69a921ec5b2fa23c0b1a80f8e1722adefd006a08
Seongjin Ahn <sjan137@naver.com>
2021-07-10 14:41:50 +0900

    210710_solved

--- math_prob_01/prob_09_Fly me to the Alpha Centauri.py
+++ math_prob_01/prob_09_Fly me to the Alpha Centauri.py
@@ -4,18 +4,11 @@ T = int(sys.stdin.readline())
 for t in range(T):
     x, y = map(int, sys.stdin.readline().split())
     distance = y - x
-    number = 0
-    start = [0]
-    while distance != number:
-        if str(3) in start:
-            index_3 = start.index('3')
-            start[index_3] = '2'
-            try:
-                start[index_3 + 1] = str(int(start[index_3 + 1]) + 1)
-            except:
-                start.append('1')
-        else:
-            start[0] = str(int(start[0]) + 1)
-        number += 1
-    start_num = [int(a) for a in start]
-    print(sum(start_num))
+    digit_num = 0
+    acc_num = 0
+    while acc_num < distance:
+        digit_num += 1
+        acc_num += (2 * digit_num + 1)
+    if distance == (acc_num - 2 * digit_num): print(2 * digit_num - 1)
+    elif distance <= (acc_num - digit_num): print(2 * digit_num)
+    else: print(2 * digit_num + 1)
