e88cfecfccac6f3af31dfc746ab1bd2a34182b4c
Seongjin Ahn <sjan137@naver.com>
2021-07-11 19:57:19 +0900

    210711_solved

--- math_prob_02/prob_03_소인수분해.py
+++ math_prob_02/prob_03_소인수분해.py
@@ -1,28 +1,11 @@
 N = int(input())
-odd_list = list(range(3, N//2+1, 2))
-sosu_list = odd_list[:]
+n = 2
 result = list()
-
-for odd in odd_list:
-    start = 2
-    while start <= (odd // 2):
-        if odd % start == 0:
-            sosu_list.remove(odd)
-            break
-        start += 1
-sosu_list.insert(0, 2)
-
 while N != 1:
-    for sosu in sosu_list:
-        if N % sosu == 0:
-            result.append(sosu)
-            N = N // sosu
-            continue
-        else: sosu_list.remove(sosu)
-    if len(sosu_list) == 0: break
+    if N % n == 0:
+        result.append(n)
+        N = N // n
+    else: n += 1
 
-if len(result) == 0: print(N)
-else:
-    result.sort()
-    for i in result:
-        print(i)
+for i in result:
+    print(i)
