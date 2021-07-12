77054dcb278ee9b819858f3a52304f22d2203521
Seongjin Ahn <sjan137@naver.com>
2021-07-12 17:22:40 +0900

    210721_solved

--- math_prob_02/prob_04_소수 구하기.py
+++ math_prob_02/prob_04_소수 구하기.py
@@ -2,17 +2,11 @@ import sys
 
 M, N = map(int, sys.stdin.readline().split())
 
-if M <= 2:
-    M = 2
+isSosu = [False] * 2 + [True] * (N - 1)
+for i in range(2, int(N**0.5)+1):
+    if isSosu[i]:
+        for j in range(2*i, N+1, i):
+            isSosu[j] = False
 
-sosu_list = list(range(2, N+1))
-start = 0
-while sosu_list[start] <= int(N**0.5):
-    for sosu in sosu_list[start+1:]:
-        if sosu % sosu_list[start] == 0: sosu_list.remove(sosu)
-    print(sosu_list[start])
-    start += 1
-    print(sosu_list)
-
-for sosu in sosu_list:
-    if sosu >= M: print(sosu)
+for i in range(M, N+1):
+    if isSosu[i]: print(i)
