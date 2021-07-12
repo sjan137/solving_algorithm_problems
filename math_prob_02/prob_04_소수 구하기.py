ec681c3375b688b54b25e8917b5785b5f8ebd777
Seongjin Ahn <sjan137@naver.com>
2021-07-12 17:03:47 +0900

    Update prob_04_소수 구하기.py

--- math_prob_02/prob_04_소수 구하기.py
+++ math_prob_02/prob_04_소수 구하기.py
@@ -2,21 +2,17 @@ import sys
 
 M, N = map(int, sys.stdin.readline().split())
 
-# 초기 설정
-sosu_list = list()
-if (M <= 2) and (N >= 2):
-    sosu_list.append(2)
-    M = 3
-if M % 2 == 0: M += 1
+if M <= 2:
+    M = 2
 
-mn_list = list(range(M, N+1, 2))
-temp_list = mn_list[:]
-for mn in mn_list:
-    start = 2
-    while start <= (mn//2):
-        if mn % start == 0: break
-        start += 1
-    if start == (mn//2)+1: sosu_list.append(mn)
+sosu_list = list(range(2, N+1))
+start = 0
+while sosu_list[start] <= int(N**0.5):
+    for sosu in sosu_list[start+1:]:
+        if sosu % sosu_list[start] == 0: sosu_list.remove(sosu)
+    print(sosu_list[start])
+    start += 1
+    print(sosu_list)
 
 for sosu in sosu_list:
-    print(sosu)
+    if sosu >= M: print(sosu)
