838a6101af6d3f50bb6cd6da8dc1ceeb185bb511
Seongjin Ahn <sjan137@naver.com>
2021-07-13 19:22:09 +0900

    210713_solved

--- math_prob_02/prob_11_터렛.py
+++ math_prob_02/prob_11_터렛.py
@@ -5,17 +5,27 @@ import math
 def whereIsRyu(dist_list):
     x1 = dist_list[0]
     y1 = dist_list[1]
-    r1 = dist_list[2]
     x2 = dist_list[3]
     y2 = dist_list[4]
-    r2 = dist_list[5]
+    if dist_list[2] < dist_list[5]:
+        r1 = dist_list[2]
+        r2 = dist_list[5]
+    else:
+        r1 = dist_list[5]
+        r2 = dist_list[2]
+        
     circle_dist = math.sqrt((y2-y1)**2 + (x2-x1)**2)
-    r_sum = r1 + r2
-    if (circle_dist == 0) & (r1 == r2): print(-1)
-    elif (circle_dist == 0) & (r1 != r2): print(0)
-    elif circle_dist < r_sum: print(2)
-    elif circle_dist == r_sum: print(1)
-    else: print(0)
+    if circle_dist == 0:
+        if r1 == r2: print(-1)
+        else: print(0)
+    elif circle_dist <= r2:
+        if (circle_dist + r1) < r2: print(0)
+        elif (circle_dist + r1) == r2: print(1)
+        else: print(2)
+    elif circle_dist > r2:
+        if circle_dist > (r1 + r2): print(0)
+        elif circle_dist == (r1 + r2): print(1)
+        else: print(2)
 
 T = int(sys.stdin.readline())
 for t in range(T):
