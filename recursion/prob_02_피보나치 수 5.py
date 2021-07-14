1dc6d1ab65427f110c7f30ff0ad9e35e2251f4d3
Seongjin Ahn <sjan137@naver.com>
2021-07-14 12:17:44 +0900

    210714_solved

---
+++ recursion/prob_02_피보나치 수 5.py
@@ -0,0 +1,11 @@
+import sys
+
+
+def fibonacci(n):
+    if n == 0: return 0
+    elif n == 1: return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+N = int(sys.stdin.readline())
+print(fibonacci(N))
