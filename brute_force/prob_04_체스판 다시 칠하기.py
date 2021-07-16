39faf304e619f35f1f6e28caa18826cef88ed27a
Seongjin Ahn <sjan137@naver.com>
2021-07-16 16:45:59 +0900

    Update prob_04_체스판 다시 칠하기.py

--- brute_force/prob_04_체스판 다시 칠하기.py
+++ brute_force/prob_04_체스판 다시 칠하기.py
@@ -2,14 +2,7 @@ import sys
 
 def minChange(m, n, board):
     # 기준 생성
-    ref = [str()] * 2
-    for _ in range(n):
-        if _ % 2 == 0:
-            ref[0] += 'W'
-            ref[1] += 'B'
-        else:
-            ref[0] += 'B'
-            ref[1] += 'W'
+    ref = ['WB', 'BW']
     
     m_start = 0
     n_start = 0
@@ -22,11 +15,11 @@ def minChange(m, n, board):
                 row = board[i]
                 for j in range(n_start, n_start+8):
                     if i % 2 == 0:
-                        result1.append(row[j] == ref[0][j])
-                        result2.append(row[j] == ref[1][j])
+                        result1.append(row[j] == ref[0][j%2])
+                        result2.append(row[j] == ref[1][j%2])
                     else:
-                        result1.append(row[j] == ref[1][j])
-                        result2.append(row[j] == ref[0][j])
+                        result1.append(row[j] == ref[1][j%2])
+                        result2.append(row[j] == ref[0][j%2])
             total_result.append(result1.count(False))
             total_result.append(result2.count(False))
             n_start += 1
