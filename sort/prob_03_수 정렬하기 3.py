f901c494bb543d96ed57995c57481fe338db8239
Seongjin Ahn <sjan137@naver.com>
2021-07-17 17:28:44 +0900

    210717_solved

--- sort/prob_03_수 정렬하기 3.py
+++ sort/prob_03_수 정렬하기 3.py
@@ -1,16 +1,12 @@
 import sys
 
 N = int(sys.stdin.readline())
-num_list = list()
-for i in range(N):
-    num_list.append(int(sys.stdin.readline()))
-num_counts = [0]
-for i in range(1, max(num_list)+1):
-    num_counts.append(num_counts[i-1] + num_list.count(i))
-result = [0] * N
-while len(num_list):
-    last = num_list.pop()
-    result[num_counts[last]-1] = last
-    num_counts[last] = num_counts[last] - 1
-for i in result:
-    print(i)
+num_counts = [0] * 10001
+for _ in range(N):
+    num_counts[int(sys.stdin.readline())] += 1
+for num in range(1, len(num_counts)):
+    if num_counts[num] == 0: continue
+    else:
+        while num_counts[num] != 0:
+            print(num)
+            num_counts[num] -= 1
