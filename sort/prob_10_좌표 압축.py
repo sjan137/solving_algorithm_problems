6269261cfa105b180136da8d4f47e145f998708b
Seongjin Ahn <sjan137@naver.com>
2021-07-21 03:17:35 +0900

    시간 초과

--- sort/prob_10_좌표 압축.py
+++ sort/prob_10_좌표 압축.py
@@ -1,18 +1,23 @@
 import sys
 
 def zipCoordinates(x_list):
-    # 입력된 x값 목록을 세트-리스트로 변환하면서 겹치는 x값 제거
     x_unique = list(set(x_list))
+    x_unique.sort()
+    # 각 원소별 개수를 딕셔너리로 저장
     count_hash = {}
-    # 각 x값마다 중복 제거한 x값 목록의 모든 값과 일일이 비교
-    for x_ref in x_unique:
-        count = 0
-        for x in x_unique:
-            if x_ref > x: count += 1
-        count_hash[x_ref] = count
+    for x in x_unique:
+        count_hash[x] = x_unique.count(x)
     
+    # 원소마다 해당 원소보다 작은 원소들의 각 개수를 합해서 딕셔너리로 저장
+    result_hash = {}
+    for i in range(len(x_unique)):
+        count = 0
+        for j in range(i):
+            count += count_hash[x_unique[j]]
+        result_hash[x_unique[i]] = count
+        
     for x in x_list:
-        print(count_hash[x], end=' ')
+        print(result_hash[x], end=' ')
 
 N = int(sys.stdin.readline())
 x_list = list(map(int, sys.stdin.readline().split()))
