4fe2100ea527e66f16e56b075502ec0be2ff63b5
Seongjin Ahn <sjan137@naver.com>
2021-07-21 03:26:55 +0900

    시간 초과

--- sort/prob_10_좌표 압축.py
+++ sort/prob_10_좌표 압축.py
@@ -2,7 +2,6 @@ import sys
 
 def zipCoordinates(x_list):
     x_unique = list(set(x_list))
-    x_unique.sort()
     # 각 원소별 개수를 딕셔너리로 저장
     count_hash = {}
     for x in x_unique:
@@ -10,11 +9,12 @@ def zipCoordinates(x_list):
     
     # 원소마다 해당 원소보다 작은 원소들의 각 개수를 합해서 딕셔너리로 저장
     result_hash = {}
-    for i in range(len(x_unique)):
+    for x_ref in x_unique:
         count = 0
-        for j in range(i):
-            count += count_hash[x_unique[j]]
-        result_hash[x_unique[i]] = count
+        for x in x_unique:
+            if x_ref <= x: continue
+            else: count += count_hash[x]
+        result_hash[x_ref] = count
         
     for x in x_list:
         print(result_hash[x], end=' ')
