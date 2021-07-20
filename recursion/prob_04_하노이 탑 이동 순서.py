cdf0d4b07e9bc992a371fccac583f13251166a6c
Seongjin Ahn <sjan137@naver.com>
2021-07-20 12:30:03 +0900

    Update prob_04_하노이 탑 이동 순서.py

--- recursion/prob_04_하노이 탑 이동 순서.py
+++ recursion/prob_04_하노이 탑 이동 순서.py
@@ -4,11 +4,13 @@ def hanoi(n):
     if n == 1: return 1
     else: return 2 * hanoi(n-1) + 1
 
-def hanoiMove(n, start):
+def hanoiMove(n):
     if n == 1:
-        return [start, 3]
+        return [1]
     else:
-        
+        return hanoiMove(n-1) + [n] + hanoiMove(n-1)
 
 N = int(sys.stdin.readline())
+layer_list = list(range(N, 0, -1))
 print(hanoi(N))
+print(hanoiMove(N))
