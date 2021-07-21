3f19c5e19922aa5e2b36f8be9be1440e1882a1a7
Seongjin Ahn <sjan137@naver.com>
2021-07-21 12:04:41 +0900

    210721_solved

--- sort/prob_10_좌표 압축.py
+++ sort/prob_10_좌표 압축.py
@@ -2,22 +2,14 @@ import sys
 
 def zipCoordinates(x_list):
     x_unique = list(set(x_list))
-    # 각 원소별 개수를 딕셔너리로 저장
+    x_unique.sort()
+    # 중복 제거한 x값 목록의 index는 해당 x보다 작은 숫자임을 의미
     count_hash = {}
-    for x in x_unique:
-        count_hash[x] = x_unique.count(x)
+    for i in range(len(x_unique)):
+        count_hash[x_unique[i]] = i
     
-    # 원소마다 해당 원소보다 작은 원소들의 각 개수를 합해서 딕셔너리로 저장
-    result_hash = {}
-    for x_ref in x_unique:
-        count = 0
-        for x in x_unique:
-            if x_ref <= x: continue
-            else: count += count_hash[x]
-        result_hash[x_ref] = count
-        
     for x in x_list:
-        print(result_hash[x], end=' ')
+        print(count_hash[x], end=' ')
 
 N = int(sys.stdin.readline())
 x_list = list(map(int, sys.stdin.readline().split()))
