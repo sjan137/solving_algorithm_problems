1975b801bfe6d11437655699dc81dd30435c0fbd
Seongjin Ahn <sjan137@naver.com>
2021-07-27 14:04:41 +0900

    210727_solved

--- back_tracking/prob_06_스도쿠.py
+++ back_tracking/prob_06_스도쿠.py
@@ -1,47 +1,44 @@
 import sys
 
-def solveSudoku(sudoku_arr, empty_list, start):
-    if start == len(empty_list):
-        # count2 = 0
-#         for empty_index in empty_list:
-            # a = empty_index[0]
-            # b = empty_index[1]
-            # sudoku_arr[a][b] = result[count2]
-            # count2 += 1
+def solveSudoku(sudoku_arr, empty_list, empty_count):
+    # 전역 변수 flag를 통해 답이 나온 상태면, 다른 경우는 모두 리턴
+    global flag
+    if flag: return
+
+    if empty_count == len(empty_list):
         for row in sudoku_arr:
             print(*row)
+        # 첫 번째 답이 나오면 flag는 True로 설정
+        flag = True
         return
     
-    check = [False] * 9
-    check_point = empty_list[start]
-    y = check_point[0]
-    x = check_point[1]
-    sudoku_col = list()
-    sudoku_sqr = list()
-    count = 0
-    for row in sudoku_arr:
-        sudoku_col.append(row[x])
-        if (count//3) == (y//3):
-            sudoku_sqr = sudoku_sqr + row[3*(x//3):3*(x//3)+3]
-        count += 1
+    # 0인 경우는 필요 없지만, 각 칸의 숫자를 바로 인덱스로 활용하기 위해 True인 상태로 추가
+    check = [True] + [False] * 9
+    # 빈 칸(0) 리스트 중 'empty_count' 번째 좌표를 y, x에 저장
+    y, x = empty_list[empty_count]
+    # 해당 좌표의 가로, 세로에 겹치는 숫자 체크
     for i in range(9):
-        if (i+1) in sudoku_arr[y]: check[i] = True
-        elif (i+1) in sudoku_col: check[i] = True
-        elif (i+1) in sudoku_sqr: check[i] = True
+        if not check[sudoku_arr[y][i]]:
+            check[sudoku_arr[y][i]] = True
+        if not check[sudoku_arr[i][x]]:
+            check[sudoku_arr[i][x]] = True
     
-    for i in range(9):
+    # 빈 칸 좌표가 포함된 3X3 박스 체크
+    b = y // 3
+    a = x // 3
+    for i in range(3*b, 3*b+3):
+        for j in range(3*a, 3*a+3):
+            if check[sudoku_arr[i][j]]: continue
+            check[sudoku_arr[i][j]] = True
+
+    for i in range(1, 10):    # check[0]은 무의미
         if check[i]: continue
-        sudoku_arr[y][x] = i+1
-        solveSudoku(sudoku_arr, empty_list, start+1)
+        sudoku_arr[y][x] = i
+        solveSudoku(sudoku_arr, empty_list, empty_count+1)
         sudoku_arr[y][x] = 0
     return
 
-sudoku_arr = list()
-empty_list = list()
-result = list()
-for _ in range(9):
-    sudoku_arr.append(list(map(int, sys.stdin.readline().split())))
-for i in range(9):
-    for j in range(9):
-        if sudoku_arr[i][j] == 0: empty_list.append([i, j])
+sudoku_arr = [list(map(int, sys.stdin.readline().split())) for _ in range(9)]
+empty_list = [(i, j) for i in range(9) for j in range(9) if sudoku_arr[i][j] == 0]
+flag = False
 solveSudoku(sudoku_arr, empty_list, 0)
