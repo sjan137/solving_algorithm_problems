a885ea29d82458b139fc436b0e24f3991e8939ee
Seongjin Ahn <sjan137@naver.com>
2021-07-29 18:17:42 +0900

    210729_solved

--- dynamic_programming_1/prob_01_피보나치 함수.py
+++ dynamic_programming_1/prob_01_피보나치 함수.py
@@ -3,23 +3,12 @@ import sys
 def fibonacci(n):
     global count_0
     global count_1
-
-    if n == 0:
-        count_0 += 1
-        return
-
-    elif n == 1:
-        count_1 += 1
-        return
-
-    else:
-        fibonacci(n-1)
-        fibonacci(n-2)
-        return
+    for _ in range(n):
+        count_0, count_1 = count_1, (count_0+count_1)
+    print(count_0, count_1)
 
 N = int(sys.stdin.readline())
 for _ in range(N):
-    count_0 = 0
+    count_0 = 1
     count_1 = 0
     fibonacci(int(sys.stdin.readline()))
-    print(count_0, count_1)
