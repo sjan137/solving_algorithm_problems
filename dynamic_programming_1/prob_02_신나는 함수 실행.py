2c773a94fb9201d14fe0cb89981c47e52672cd87
Seongjin Ahn <sjan137@naver.com>
2021-07-30 15:31:18 +0900

    210730_solved

--- dynamic_programming_1/prob_02_신나는 함수 실행.py
+++ dynamic_programming_1/prob_02_신나는 함수 실행.py
@@ -1,44 +1,25 @@
 import sys
 
-def recurW(a, b, c):
-    if a <= 0 or b <= 0 or c<= 0:
-        return 1
-
-    elif a > 20 or b > 20 or c > 20:
-        return recurW(20, 20, 20)
-
-    elif a < b and b < c:
-        return recurW(a, b, c-1) + recurW(a, b-1, c-1) - recurW(a, b-1, c)
-    
-    else:
-        return recurW(a-1, b, c) + recurW(a-1, b-1, c) + recurW(a-1, b, c-1) - recurW(a-1, b-1, c-1)
-
 # dynamic programming W
 def dpW(a, b, c):
     if a <= 0 or b <= 0 or c<= 0:
         a = b = c = 0
+        val[a][b][c] = 1
     elif a > 20 or b > 20 or c> 20:
         a = b = c = 20
     
-    # val = [[[0] * (c+1)] * (b+1)] * (a+1)
-    val = []
-    a += 1
-    b += 1
-    c += 1
-    for i in range(a):
-        for j in range(b):
-            for k in range(c):
-                p = i * (b) * (c)
-                q = j * (c)
-                r = k
-                if i == 0 or j == 0 or k == 0: val.append(1)
-                elif i < j and j < k: val.append(val[p + q + r-1] + val[p + q-c + r-1] - val[p + q-c + r])
-                else: val.append(val[p-b*c + q + r] + val[p-b*c + q-c + r] + val[p-b*c + q + r-1] - val[p-b*c + q-c + r-1])
-    # print(len(val), val)
-    return val.pop()
+    if val[a][b][c]: return val[a][b][c]
+    else:
+        if a < b and b < c:
+            val[a][b][c] = dpW(a, b, c-1) + dpW(a, b-1, c-1) - dpW(a, b-1, c)
+            return val[a][b][c]
+        else:
+            val[a][b][c] = dpW(a-1, b, c) + dpW(a-1, b-1, c) + dpW(a-1, b, c-1) - dpW(a-1, b-1, c-1)
+            return val[a][b][c]
 
+max_num = 21
+val = [[[0] * max_num for _ in range(max_num)] for _ in range(max_num)]
 while True:
     a, b, c = map(int, sys.stdin.readline().split())
     if a == b == c == -1: break
-    # print(recurW(a, b, c))
-    print(dpW(a, b, c))
+    print('w(%d, %d, %d) = %d' % (a, b, c, dpW(a, b, c)))
