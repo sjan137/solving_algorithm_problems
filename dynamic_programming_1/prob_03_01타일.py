510c6ee45b9367ef82ea4427b52285197231d503
Seongjin Ahn <sjan137@naver.com>
2021-08-01 15:25:21 +0900

    210801_solved

--- dynamic_programming_1/prob_03_01타일.py
+++ dynamic_programming_1/prob_03_01타일.py
@@ -1,27 +1,12 @@
 import sys
 
-# 결과를 순열의 합으로 계산
 def binaryTile(n):
-    global count
-    if n == 1: return
-    a = n - 1
-    b = 1
-    while a >= b:
-        count += combination(a, b)
-        a -= 1
-        b += 1
-    return(count)
-
-def combination(a, b):
-    result = 1
-    c = a - b
-    for i in range(b, a):
-        result *= (i+1)
-    while c > 0:
-        result /= c
-        c -= 1
-    return int(result)
+    if n < 3: return n
+    else:
+        first, second = 1, 2
+        for _ in range(n-2):
+            first, second = second, (first + second) % 15746
+        return second
 
 N = int(sys.stdin.readline())
-count = 1
 print(binaryTile(N))
