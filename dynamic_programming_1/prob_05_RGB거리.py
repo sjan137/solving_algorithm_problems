b3f1bafed123bb487317e20743debfd26ca1cc1a
Seongjin Ahn <sjan137@naver.com>
2021-08-02 17:06:54 +0900

    시간 초과_조금은 속도 향상됨

--- dynamic_programming_1/prob_05_RGB거리.py
+++ dynamic_programming_1/prob_05_RGB거리.py
@@ -1,30 +1,30 @@
 import sys
 
-def streetRGB(n, rgb_arr, start):
+def streetRGB(n, rgb_arr, start, acc_sum):
+    global rgb_price
+    if acc_sum >= rgb_price: return
+
     if start == n:
-        price = 0
-        for i in range(n):
-            price += rgb_arr[i][result[i]]
-        rgb_price.append(price)
+        rgb_price = acc_sum
         return
-    
+
     for i in range(3):
         if not result:
             result.append(i)
-            streetRGB(n, rgb_arr, start+1)
+            streetRGB(n, rgb_arr, start+1, acc_sum+rgb_arr[start][i])
             result.pop()
         else:
             if result[-1] == i: continue
             result.append(i)
-            streetRGB(n, rgb_arr, start+1)
+            streetRGB(n, rgb_arr, start+1, acc_sum+rgb_arr[start][i])
             result.pop()
     return
 
 N = int(sys.stdin.readline())
 rgb_array = []
-rgb_price = []
+rgb_price = 1000 * 1000
 result = []
 for _ in range(N):
     rgb_array.append(list(map(int, sys.stdin.readline().split())))
-streetRGB(N, rgb_array, 0)
-print(min(rgb_price))
+streetRGB(N, rgb_array, 0, 0)
+print(rgb_price)
