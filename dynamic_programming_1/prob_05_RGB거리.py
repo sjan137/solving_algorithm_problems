9720d923f46471518f31962dcbe209deceee2d10
Seongjin Ahn <sjan137@naver.com>
2021-08-03 23:35:19 +0900

    오답

--- dynamic_programming_1/prob_05_RGB거리.py
+++ dynamic_programming_1/prob_05_RGB거리.py
@@ -6,25 +6,24 @@ def streetRGB(n, rgb_arr, start, acc_sum):
 
     if start == n:
         rgb_price = acc_sum
+        print(rgb_price, result)
         return
 
     for i in range(3):
-        if not result:
-            result.append(i)
-            streetRGB(n, rgb_arr, start+1, acc_sum+rgb_arr[start][i])
-            result.pop()
-        else:
-            if result[-1] == i: continue
-            result.append(i)
-            streetRGB(n, rgb_arr, start+1, acc_sum+rgb_arr[start][i])
-            result.pop()
+        if check[i]:
+            check[i] = False
+            continue
+        check[i] = True
+        result.append(i)
+        streetRGB(n, rgb_arr, start+1, acc_sum+rgb_arr[start][i])
+        result.pop()
+        check[i] = False
     return
 
 N = int(sys.stdin.readline())
-rgb_array = []
 rgb_price = 1000 * 1000
+check = [False] * 3
 result = []
-for _ in range(N):
-    rgb_array.append(list(map(int, sys.stdin.readline().split())))
+rgb_array = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 streetRGB(N, rgb_array, 0, 0)
 print(rgb_price)
