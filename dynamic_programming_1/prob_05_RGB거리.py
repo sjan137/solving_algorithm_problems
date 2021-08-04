fb45ee49f6b05466dd9f2b1697bcce71907ebae3
Seongjin Ahn <sjan137@naver.com>
2021-08-04 22:31:05 +0900

    210804_solved

--- dynamic_programming_1/prob_05_RGB거리.py
+++ dynamic_programming_1/prob_05_RGB거리.py
@@ -1,29 +1,16 @@
 import sys
 
-def streetRGB(n, rgb_arr, start, acc_sum):
-    global rgb_price
-    if acc_sum >= rgb_price: return
 
-    if start == n:
-        rgb_price = acc_sum
-        print(rgb_price, result)
-        return
-
-    for i in range(3):
-        if check[i]:
-            check[i] = False
-            continue
-        check[i] = True
-        result.append(i)
-        streetRGB(n, rgb_arr, start+1, acc_sum+rgb_arr[start][i])
-        result.pop()
-        check[i] = False
-    return
+def street_rgb(n, rgb_price, check):
+    for i in range(n):
+        for j in range(3):
+            if i == 0:
+                check[i][j] = rgb_price[i][j]
+            else:
+                check[i][j] = min(check[i-1][(j+1)%3], check[i-1][(j+2)%3]) + rgb_price[i][j]
+    return min(check[n-1])
 
 N = int(sys.stdin.readline())
-rgb_price = 1000 * 1000
-check = [False] * 3
-result = []
-rgb_array = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-streetRGB(N, rgb_array, 0, 0)
-print(rgb_price)
+rgb_arr = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+arr_check = [[0] * 3 for _ in range(N)]
+print(street_rgb(N, rgb_arr, arr_check))
