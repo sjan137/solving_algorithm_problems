e11e8e204f4c850f0174c675f84378da3601446f
Seongjin Ahn <sjan137@naver.com>
2021-08-05 22:58:43 +0900

    210805_solved

--- dynamic_programming_1/prob_07_계단 오르기.py
+++ dynamic_programming_1/prob_07_계단 오르기.py
@@ -1,38 +1,16 @@
 import sys
 
-
-def step_stairs(n, stairs_score, start):
-    if start == n:
-        score_sum_list.append(sum(score_sum))
-        return None
-
-    for i in range(2):
-        if i == 0:
-            if start < 2:
-                if len(one_limit) == 2:
-                    one_limit.pop()
-                    one_limit.pop()
-                    continue
-                else:
-                    one_limit.append(1)
-            else:
-                if one_limit:
-                    one_limit.pop()
-                    continue
-                else:
-                    one_limit.append(1)
-        start += (i+1)
-        if start > n: return None
-        score_sum.append(stairs_score[start])
-        step_stairs(n, stairs_score, start)
-        start -= (i+1)
-        score_sum.pop()
-    return None
+# 각 계단 위치에 따라 내가 얻을 수 있는 최대값을 차근차근 구해가기
+def step_stairs(n, stairs_score):
+    score_sum = [0] * n
+    score_sum[0] = stairs_score[0]
+    if n >= 2: score_sum[1] = score_sum[0] + stairs_score[1]
+    if n >= 3: score_sum[2] = max(score_sum[0], stairs_score[1]) + stairs_score[2]
+    if n >= 4:
+        for i in range(3, n):
+            score_sum[i] = stairs_score[i] + max(score_sum[i-2], score_sum[i-3] + stairs_score[i-1])
+    print(score_sum[n-1])
 
 N = int(sys.stdin.readline())
-stairs_score = [0] + [int(sys.stdin.readline()) for _ in range(N)]
-one_limit = []
-score_sum = []
-score_sum_list = []
-step_stairs(N, stairs_score, 0)
-print(max(score_sum_list))
+stairs_score = [int(sys.stdin.readline()) for _ in range(N)]
+step_stairs(N, stairs_score)
