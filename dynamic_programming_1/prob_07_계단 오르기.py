d54acf5d19b4e5dd0322f87ca62698cca4f99a91
Seongjin Ahn <sjan137@naver.com>
2021-08-05 23:01:59 +0900

    변수명 수정

--- dynamic_programming_1/prob_07_계단 오르기.py
+++ dynamic_programming_1/prob_07_계단 오르기.py
@@ -2,14 +2,14 @@ import sys
 
 # 각 계단 위치에 따라 내가 얻을 수 있는 최대값을 차근차근 구해가기
 def step_stairs(n, stairs_score):
-    score_sum = [0] * n
-    score_sum[0] = stairs_score[0]
-    if n >= 2: score_sum[1] = score_sum[0] + stairs_score[1]
-    if n >= 3: score_sum[2] = max(score_sum[0], stairs_score[1]) + stairs_score[2]
+    dp = [0] * n
+    dp[0] = stairs_score[0]
+    if n >= 2: dp[1] = dp[0] + stairs_score[1]
+    if n >= 3: dp[2] = max(dp[0], stairs_score[1]) + stairs_score[2]
     if n >= 4:
         for i in range(3, n):
-            score_sum[i] = stairs_score[i] + max(score_sum[i-2], score_sum[i-3] + stairs_score[i-1])
-    print(score_sum[n-1])
+            dp[i] = stairs_score[i] + max(dp[i-2], dp[i-3] + stairs_score[i-1])
+    print(dp[n-1])
 
 N = int(sys.stdin.readline())
 stairs_score = [int(sys.stdin.readline()) for _ in range(N)]
