d87c1a4898ec71386bcab11527139adcd2f9d9e8
Seongjin Ahn <sjan137@naver.com>
2021-08-06 23:35:12 +0900

    210806_solved by dp

--- dynamic_programming_1/prob_08_1로 만들기.py
+++ dynamic_programming_1/prob_08_1로 만들기.py
@@ -3,27 +3,10 @@ import sys
 
 def solve(n):
     count = [0] * n
-    num_list = []
-    min_count = 2
-
-    if n >= 2:
-        count[1] = 1
-        num_list.append(2)
-    if n >= 3:
-        count[2] = 1
-        num_list.append(3)
-    
-    while num_list:
-        new_set = set()
-        for i in num_list:
-            a, b, c = i+1, i*2, i*3
-            for j in [a, b, c]:
-                if j > n or count[j-1]: continue
-                else:
-                    count[j-1] = min_count
-                    new_set.add(j)
-        num_list = list(new_set)
-        min_count += 1
+    if n >= 2: count[1] = 1
+    if n >= 3: count[2] = 1
+    for i in range(4, n+1):
+        count[i-1] = 1 + min(count[i//2-1]+i%2, count[i//3-1]+i%3)
     return count[-1]
 
 N = int(sys.stdin.readline())
