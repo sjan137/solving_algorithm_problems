5a6cd23225deeab2a5cd7d5272b99e0954197b5f
Seongjin Ahn <sjan137@naver.com>
2021-08-07 21:31:48 +0900

    210807_solved

--- dynamic_programming_1/prob_09_쉬운 계단 수.py
+++ dynamic_programming_1/prob_09_쉬운 계단 수.py
@@ -2,24 +2,22 @@ import sys
 
 
 def solve(n):
-    if len(stair_num) == n:
-        result.append(1)
-        return None
-
-    for i in range(10):
-        if not stair_num:
-            if i == 0: continue
-            stair_num.append(i)
-            solve(n)
-            stair_num.pop()
-        elif abs(stair_num[-1] - i) == 1:
-            stair_num.append(i)
-            solve(n)
-            stair_num.pop()
-    return None
+    count = [[0] * 10 for _ in range(n)]
+    for i in range(n):
+        if i == 0:
+            for j in range(1, 10):
+                count[i][j] += 1
+        else:
+            for j in range(10):
+                a = count[i-1][j]
+                b = stair_dict[str(j)]
+                for k in b:
+                    count[i][k] += a
+    return sum(count[-1])
 
 N = int(sys.stdin.readline())
-stair_num = []
-result = []
-solve(N)
-print(len(result))
+stair_dict = {'0': [1], '9': [8]}
+for i in range(1, 9):
+    stair_dict[str(i)] = [i-1, i+1]
+to_div = 10 ** 9
+print(solve(N) % to_div)
