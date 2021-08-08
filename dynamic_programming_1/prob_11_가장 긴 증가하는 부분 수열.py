3a6aa7abe383f46a36012054cae77ba0f614891c
Seongjin Ahn <sjan137@naver.com>
2021-08-09 02:07:36 +0900

    210809_solved

--- dynamic_programming_1/prob_11_가장 긴 증가하는 부분 수열.py
+++ dynamic_programming_1/prob_11_가장 긴 증가하는 부분 수열.py
@@ -1,21 +1,16 @@
 import sys
 
-# 재귀 함수로 풀어본 버전
-def solve(n, n_list, start):
-    if start == (n-1):
-        if len(temp) > result[0]: result[0] = len(temp)
-    
-    if not temp: temp.append(n_list[start])
-    for i in range(start+1, n):
-        if temp[-1] >= n_list[i]: continue
-        temp.append(n_list[i])
-        solve(n, n_list, i)
-        temp.pop()
-    return
+# 동적 프로그래밍으로 풀어본 버전
+def solve(n, n_list):
+    dp = [1] + [0] * (n-1)
+    for i in range(1, n):
+        temp = 0
+        for j in range(i):
+            if n_list[i] > n_list[j]:
+                if temp < dp[j]: temp = dp[j]
+        dp[i] = temp + 1
+    return max(dp)
 
 N = int(sys.stdin.readline())
 num_list = list(map(int, sys.stdin.readline().split()))
-temp = []
-result = [0]
-solve(N, num_list, 0)
-print(result[0])
+print(solve(N, num_list))
