e7e81fcff57a7c91940cd3640c969583f483072d
Seongjin Ahn <sjan137@naver.com>
2021-08-08 15:24:05 +0900

    Update prob_10_포도주 시식.py
    불필요한 부분 삭제

--- dynamic_programming_1/prob_10_포도주 시식.py
+++ dynamic_programming_1/prob_10_포도주 시식.py
@@ -3,11 +3,11 @@ import sys
 
 def solve(n, dp):
     max_drink = [0] * n
+    
     if n >= 1: max_drink[0] = dp[0]
     if n >= 2: max_drink[1] = sum(dp[:2])
     if n >= 3:
-        min_in_three = dp.index(min(dp[:3]))
-        max_drink[2] = sum(dp[:min_in_three]) + sum(dp[min_in_three+1:3])
+        max_drink[2] = max(dp[2]+dp[1], dp[2]+dp[0], dp[1]+dp[0])
     if n >= 4:
         max_drink[3] = max(dp[3]+dp[2]+dp[0], dp[3]+dp[1]+dp[0], dp[2]+dp[1])
     if n >= 5:
