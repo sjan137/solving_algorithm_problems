16d41909afd2cfe24c402ba7719470809a2d1b87
Seongjin Ahn <sjan137@naver.com>
2021-08-13 00:47:24 +0900

    210813_solved

--- dynamic_programming_1/prob_14_LCS.py
+++ dynamic_programming_1/prob_14_LCS.py
@@ -10,22 +10,17 @@ def getLCS(str1, str2):
     if str1[0] == str2[0]: acc_arr[0][0] = 1  # [0][0] 값 설정
 
     for i in range(y):
-        flag = False
         for j in range(x):
-            if j == 0:  # 1열일 때는 위의 값을 가져옴. 같다면 +1. 단, 이미 1이면 그냥 패스
-                if i == 0:
-                    if acc_arr[0][0]: flag = True
-                    continue  # 이미 [0][0] 값은 구해서 할당함.
+            if j == 0:  # 1열은 위의 값을 가져옴. 만약 문자가 같다면, 1을 넘지 않는 선에서 +1
+                if i == 0: continue  # 1열 1행 값은 이미 정했으므로 패스
                 acc_arr[i][j] = acc_arr[i-1][j]
-                if acc_arr[i][j]: continue  # 1열의 값은 이미 1이라면 같은 문자여도 통과
-                elif str1[j] == str2[i] and not flag:
-                    flag = True
+                if str1[j] == str2[i] and acc_arr[i][j] < i+1 and acc_arr[i][j] < j+1:
                     acc_arr[i][j] += 1
-            else:  # 아닐 때는 위쪽과 왼쪽 중 큰 값을 가져옴. 같다면 +1 하면서 
-                acc_arr[i][j] = max(acc_arr[i][j-1], acc_arr[i-1][j])
-                if str1[j] == str2[i] and not flag:
-                    flag = True
-                    acc_arr[i][j] += 1
+            else:  # 1열 외의 다른 열은 바로 직전 행의 값과 직전 열의 값 중 큰 값을 가져옴.
+                if i == 0: acc_arr[i][j] = acc_arr[i][j-1]  # 단, 직전 행이 없으므로 1행은 직전 열의 값을 가져옴.
+                else: acc_arr[i][j] = max(acc_arr[i-1][j], acc_arr[i][j-1])
+                if str1[j] == str2[i] and acc_arr[i][j] < i+1 and acc_arr[i][j] < j+1:
+                    acc_arr[i][j] = acc_arr[i-1][j-1] + 1
     return acc_arr
 
 string_1 = sys.stdin.readline().strip()
