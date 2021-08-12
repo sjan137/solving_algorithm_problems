8e3832923f56dab9deca8ae3f229c55d9cfc0af1
Seongjin Ahn <sjan137@naver.com>
2021-08-12 23:38:49 +0900

    LCS 풀이 방식 조금 구현

--- dynamic_programming_1/prob_14_LCS.py
+++ dynamic_programming_1/prob_14_LCS.py
@@ -2,24 +2,32 @@ import sys
 
 
 def getLCS(str1, str2):
-    # str1을 기준으로 할 때
-    len_1 = len(str1)
-    len_2 = len(str2)
-    for i in range(len_1):
-        list_1 = []
-        list_2 = []
-        for j in range(len_2):
-            if str1[i] == str2[j]:
-                if not list_1:
-                    list_1.append(i)
-                    list_2.append(j)
-                elif j > list_2[-1]:
-                    list_1.append(i)
-                    list_2.append(j)
-        print(list_1, list_2)
-    return
+    # 두 단어의 길이를 각각 x축, y축으로 하며, 누적하여 공통 부분 숫자를 더하는 배열 생성
+    x = len(str1)
+    y = len(str2)
+    acc_arr = [[0] * x for _ in range(y)]
+
+    if str1[0] == str2[0]: acc_arr[0][0] = 1  # [0][0] 값 설정
+
+    for i in range(y):
+        flag = False
+        for j in range(x):
+            if j == 0:  # 1열일 때는 위의 값을 가져옴. 같다면 +1. 단, 이미 1이면 그냥 패스
+                if i == 0:
+                    if acc_arr[0][0]: flag = True
+                    continue  # 이미 [0][0] 값은 구해서 할당함.
+                acc_arr[i][j] = acc_arr[i-1][j]
+                if acc_arr[i][j]: continue  # 1열의 값은 이미 1이라면 같은 문자여도 통과
+                elif str1[j] == str2[i] and not flag:
+                    flag = True
+                    acc_arr[i][j] += 1
+            else:  # 아닐 때는 위쪽과 왼쪽 중 큰 값을 가져옴. 같다면 +1 하면서 
+                acc_arr[i][j] = max(acc_arr[i][j-1], acc_arr[i-1][j])
+                if str1[j] == str2[i] and not flag:
+                    flag = True
+                    acc_arr[i][j] += 1
+    return acc_arr
 
 string_1 = sys.stdin.readline().strip()
 string_2 = sys.stdin.readline().strip()
-print(string_1, string_2)
 print(getLCS(string_1, string_2))
