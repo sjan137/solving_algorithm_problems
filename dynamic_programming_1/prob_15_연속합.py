74ed27c8e9faadb1cfc0a12bf2d45f275b306b10
Seongjin Ahn <sjan137@naver.com>
2021-08-14 00:40:33 +0900

    시간 초과
    최댓값을 저장하는 변수 하나에 계속해서 값을 비교하며 큰 값이면 저장하는 방식

--- dynamic_programming_1/prob_15_연속합.py
+++ dynamic_programming_1/prob_15_연속합.py
@@ -2,17 +2,13 @@ import sys
 
 
 def solve(n, num_list):
-    result = [[-1 * (10 ** 8)] * n for _ in range(n)]
-
-    for i in range(n):
-        for j in range(i, n):
-            if i == j: result[i][i] = num_list[j]
-            else:
-                result[i][j] = result[i][j-1] + num_list[j]
-
     max_sum = -1 * (10 ** 8)
-    for row in result:
-        if max(row) > max_sum: max_sum = max(row)
+
+    for i in range(1, n):
+        for j in range(i, n):
+            num_sum = sum(num_list[j-i:j])
+            if max_sum < num_sum: max_sum = num_sum
+
     return max_sum
 
 N = int(sys.stdin.readline())
