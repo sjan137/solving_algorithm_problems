ef1ed77ee73a8dc3d5ef886961858b2455e635da
Seongjin Ahn <sjan137@naver.com>
2021-08-15 15:30:50 +0900

    210815_solved
    226652KB, 4608ms

--- dynamic_programming_1/prob_16_평범한 배낭.py
+++ dynamic_programming_1/prob_16_평범한 배낭.py
@@ -2,15 +2,16 @@ import sys
 
 # Knapsack 알고리즘(0-1 Knapsack)
 def solve(n, k, wv_list):
-    dp = [[0] * k for _ in range(n)]
+    dp = [[0] * (k+1) for _ in range(n+1)]
+    new_wv_list = [[0, 0]] + wv_list
 
-    for i in range(k):
-        for j in range(n):
-            now_w, now_v = wv_list[j]
-            if now_w <= i+1: dp[j][i] = max(dp[j-1][i], now_v + dp[j-1][i-now_w])
-            else: dp[j][i] = dp[j-1][i]
+    for i in range(1, n+1):
+        for j in range(1, k+1):
+            now_w, now_v = new_wv_list[i]
+            if now_w <= j: dp[i][j] = max(dp[i-1][j], now_v + dp[i-1][j-now_w])
+            else: dp[i][j] = dp[i-1][j]
 
-    return dp
+    return dp[-1][-1]
 
 N, K = map(int, sys.stdin.readline().split())
 WV = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
