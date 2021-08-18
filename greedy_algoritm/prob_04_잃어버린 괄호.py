409bb7d57b3418a6ea6c42950e9fab97b279ed50
Seongjin Ahn <sjan137@naver.com>
2021-08-19 00:59:43 +0900

    210819_solved
    메모리 동일(29200KB)
    시간 72ms -> 80ms
    코드 길이 669B -> 289B

--- greedy_algoritm/prob_04_잃어버린 괄호.py
+++ greedy_algoritm/prob_04_잃어버린 괄호.py
@@ -3,22 +3,11 @@ import sys
 
 def main():
     # 한 번 마이너스가 나온 뒤에는 괄호를 통해 다 마이너스 연산이 되도록 할 수 있음.
-    line = sys.stdin.readline()
-    flag = False
-    plus_count = 0
-    for digit in line:
-        if not flag:
-            if digit == '+': plus_count += 1
-            elif digit == '-': flag = True
-    
-    new_line = list(map(int, line.replace('+', ' ').replace('-', ' ').split()))
-    result = 0
-    plus_count += 1
-    for num in new_line:
-        if plus_count:
-            result += num
-            plus_count -= 1
-        else: result -= num
+    line_to_cal = sys.stdin.readline().split('-')
+    result = sum(map(int, line_to_cal[0].split('+')))
+
+    for line in line_to_cal[1:]:
+        result -= sum(map(int, line.split('+')))
     
     print(result)
     return None
