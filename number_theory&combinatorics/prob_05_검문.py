6afa7efa9e8f161ea6d7d167066304a879d1c8a1
Seongjin Ahn <sjan137@naver.com>
2021-08-23 01:14:36 +0900

    시간 줄이기
    함수의 끝 부분에서 for문의 범위를 줄이고 한 번에 나누는 수와 몫을 저장하려고 하면 아예 틀렸다고 나옴.

--- number_theory&combinatorics/prob_05_검문.py
+++ number_theory&combinatorics/prob_05_검문.py
@@ -5,7 +5,6 @@ def gcd(a, b):
     return gcd(b%a, a) if b%a else a
 
 def solve(n, n_list):
-    result = []
     gcd_list = []
     n_list.sort()
     
@@ -13,14 +12,22 @@ def solve(n, n_list):
     if n == 2: gcd_list.append(n_list[1]-n_list[0])
     else:
         for i in range(1, n-1):
-            gcd_list.append(gcd(n_list[i+1]-n_list[0], n_list[i]-n_list[0]))
-    gcd_list.sort()
+            temp = gcd(n_list[i+1]-n_list[i], n_list[i]-n_list[i-1])
+            if temp not in gcd_list: gcd_list.append(temp)
 
     # 최대공약수 사이의 최대공약수를 구해서 가장 공통적인 최대공약수 구하기
     gcd_num = gcd_list[0]
-    for i in range(1, n-2):
+    for i in range(1, len(gcd_list)):
         gcd_num = gcd(gcd_num, gcd_list[i])
 
+    # result = [gcd_num]
+    # for i in range(2, int(gcd_num**0.5)+1):
+    #     if gcd_num % i == 0: 
+    #         result.append(i)
+    #         result.append(gcd_num//i)
+    # result = list(set(result))
+    # print(*result)
+    result = []
     for i in range(2, gcd_num+1):
         if gcd_num % i == 0: result.append(i)
     print(*result)
