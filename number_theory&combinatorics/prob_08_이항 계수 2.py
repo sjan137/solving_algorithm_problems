8ddc0d1d738cb1ce2e1d073097bf0dc67fe91175
Seongjin Ahn <sjan137@naver.com>
2021-08-25 01:17:08 +0900

    210825_solved
    149708KB, 408ms
    (반면, math 라이브러리의 팩토리얼을 이용하면 31312KB, 100ms)

--- number_theory&combinatorics/prob_08_이항 계수 2.py
+++ number_theory&combinatorics/prob_08_이항 계수 2.py
@@ -1,13 +1,15 @@
 import sys
-from math import factorial
 
+# 파스칼의 삼각형을 통해 정확히 계산
 def main():
     N, K = map(int, sys.stdin.readline().split())
-    newK = min(K, N-K)
-    result = 1
-    for i in range(newK):
-        result = result * (N-i) / (i+1)
-    print(int(result)%10007)
+    comb_arr = [[1] * (N+1) for _ in range(N+1)]
+
+    for i in range(1, N-K+1):  # 최소한의 행까지만 계산
+        for j in range(1, N+1):
+            comb_arr[i][j] = comb_arr[i][j-1] + comb_arr[i-1][j]
+    
+    print(comb_arr[N-K][K] % 10007)
     return None
 
 if __name__ == '__main__':
