38f3f8665de21ee427a534490c04cc3335214afe
Seongjin Ahn <sjan137@naver.com>
2021-08-31 20:06:16 +0900

    Create prob_2630_색종이 만들기.py
    아무리 봐도 맞는 것 같은데, 틀렸다고 뜨는 이유를 모르겠음.

---
+++ divide_and_conquer/prob_2630_색종이 만들기.py
@@ -0,0 +1,45 @@
+import sys
+
+# 색종이를 나누고, 나눈 색종이의 각 개수를 반환
+# 입력 받은 색종이가 단색이라면, (1, 0)-하양 또는 (0, 1)-파랑 형태로 반환
+# 단색이 될 때까지 좌우로 나눔
+def dividePaper(paper, n):
+    result = []
+    if n == 1 or isOneColor(paper):
+        if paper[0][0] == 0: return [(1, 0)]
+        else: return [(0, 1)]
+    
+    mid = n // 2
+    d1 = [row[:mid] for row in paper[:mid]]
+    d2 = [row[mid:] for row in paper[:mid]]
+    d3 = [row[:mid] for row in paper[mid:]]
+    d4 = [row[mid:] for row in paper[mid:]]
+
+    for d in [d1, d2, d3, d4]:
+        result += dividePaper(d, mid)
+    return result
+
+def isOneColor(paper):
+    n = len(paper)
+    ref = paper[0][0]
+    
+    for i in range(n):
+        for j in range(n):
+            if paper[i][j] != ref: return False
+    return True
+
+def main():
+    N = int(sys.stdin.readline())
+    color_paper = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+    color_info = dividePaper(color_paper, N)
+    white = 0
+    blue = 0
+    
+    for w, b in color_info:
+        white += w
+        blue += b
+    
+    return str(white) + str(blue)
+
+if __name__ == '__main__':
+    print('\n'.join(main()))
