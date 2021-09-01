2eb61a109b3845bf0d8f6d67f1248efc71ba7033
Seongjin Ahn <sjan137@naver.com>
2021-09-01 11:53:22 +0900

    Update prob_11401_이항 계수 3.py

--- divide_and_conquer/prob_11401_이항 계수 3.py
+++ divide_and_conquer/prob_11401_이항 계수 3.py
@@ -3,6 +3,7 @@ import sys
 # 페르마의 소정리 이용
 # 소수인 p, p의 배수가 아닌 A에 대해 A^(p-1) === 1(mod p) - p로 나눈 나머지가 1
 # 즉, (A^(p-2))%p = (A^(-1))%p
+# 이를 이용해서 이항 계수의 분모로 인한 나눗셈을 곱셈으로 변환
 def fermaPower(b, p, origin_p):
     if p == 1: return b % origin_p
     elif p % 2: return (b * (fermaPower(b, p//2, origin_p) ** 2)) % origin_p
@@ -15,9 +16,7 @@ def main():
     factorialList = [1 for _ in range(N+1)]
     for i in range(2, N+1):
         factorialList[i] = (i * factorialList[i-1]) % P
-    A, B = factorialList[N], factorialList[K]
-    result = ((A%P) * (fermaPower(B, 1000000005, P)%P)) % P
-    return result
+    A, B = factorialList[N], (factorialList[K] * factorialList[N-K]) % P
+    return ((A%P) * (fermaPower(B, P-2, P)%P)) % P
 
-if __name__ == "__main__":
-    print(main())
+if __name__ == "__main__": print(main())
