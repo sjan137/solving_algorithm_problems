e6bed2d3f7849b7bdb676faebd8fdc9c564ca5e1
Seongjin Ahn <sjan137@naver.com>
2021-09-07 00:49:25 +0900

    Update prob_10942_팰린드롬인가.py

--- dynamic_programming/prob_10942_팰린드롬인가.py
+++ dynamic_programming/prob_10942_팰린드롬인가.py
@@ -1,10 +1,9 @@
 import sys
 
 # 팰린드롬 여부를 저장하는 2차원 리스트. i == j인 경우는 모두 1
-# data[i] == data[j]일 때, data[i+1] 부터 data[j-1]까지가 팰린드롬이라면(pal[i+1][j-1] == 1) i, j일 때도 팰린드롬
-# data[i] != data[j]일 때, 팰린드롬 아님
-def isPal(data):
-    n = len(data)
+# data[i] == data[j]일 때, data[i+1] 부터 data[j-1]까지가 팰린드롬이라면(pal[i+1][j-1] == 1), i, j일 때도 팰린드롬
+# data[i] == data[j]일 때, i부터 j까지가 같은 두 문자열( ex) aa)이라면(i + 1 == j), i, j일 때도 팰린드롬
+def isPal(data, n):
     pal = [[0] * n for _ in range(n)]
 
     for i in range(n-1, -1, -1):
@@ -12,6 +11,7 @@ def isPal(data):
             if i == j: pal[i][j] = 1
             elif data[i] == data[j]:
                 if pal[i+1][j-1]: pal[i][j] = 1
+                elif i + 1 == j: pal[i][j] = 1
     
     return pal
 
@@ -19,11 +19,12 @@ def main():
     N = int(sys.stdin.readline())
     nums = sys.stdin.readline().split()
     M = int(sys.stdin.readline())
-    pal_arr = isPal(nums)
+    pal_arr = isPal(nums, N)
     
     for _ in range(M):
         start, end = map(int, sys.stdin.readline().split())
         print(pal_arr[start-1][end-1])
+    
     return
 
 if __name__ == "__main__":
