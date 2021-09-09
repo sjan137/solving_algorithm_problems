77beb9f5eeca88835cfe3b9bad03ee1faf2748b6
Seongjin Ahn <sjan137@naver.com>
2021-09-09 20:41:13 +0900

    Update prob_11066_파일 합치기.py
    heapq로 테스트 해봤다.

--- dynamic_programming/prob_11066_파일 합치기.py
+++ dynamic_programming/prob_11066_파일 합치기.py
@@ -1,23 +1,47 @@
-import sys
+# import sys
 
-# 결국 마지막 비용은 costs의 모든 원소의 합이다.
-# 그리고 그 이전에는 costs 중 가장 큰 값 하나를 제외한 나머지의 합이다.
-# 
-def calCost(n, costs):
-    result = []
-    costs.sort()
-    for i in range(2, n+1):
-        result.append(sum(costs[:i]))
-    return sum(result)
+# # 결국 마지막 비용은 costs의 모든 원소의 합이다.
+# # 그리고 그 이전에는 costs 중 가장 큰 값 하나를 제외한 나머지의 합이다.
+# # 
+# def calCost(n, costs):
+#     result = []
+#     costs.sort()
+#     for i in range(2, n+1):
+#         result.append(sum(costs[:i]))
+#     return sum(result)
+
+# def main():
+#     T = int(sys.stdin.readline())
+#     result = []
+#     for _ in range(T):
+#         N = int(sys.stdin.readline())
+#         cost_list = list(map(int, sys.stdin.readline().split()))
+#         result.append(calCost(N, cost_list))
+#     return result
+
+# if __name__ == "__main__":
+#     print(main())
+
+import sys
+import heapq
 
 def main():
     T = int(sys.stdin.readline())
-    result = []
     for _ in range(T):
         N = int(sys.stdin.readline())
-        cost_list = list(map(int, sys.stdin.readline().split()))
-        result.append(calCost(N, cost_list))
-    return result
-
+        inputs = list(map(int, sys.stdin.readline().split()))
+    
+        result = 0
+        q = []
+        for w in inputs:
+            heapq.heappush(q, w)
+    
+        while len(q) > 1:
+            a, b = heapq.heappop(q), heapq.heappop(q)
+            heapq.heappush(q, a+b)
+            result += (a + b)
+    
+        print(result)
+    
 if __name__ == "__main__":
-    print(main())
+    main()
