072192997ce0f7240a22706c94e531cc82d58506
Seongjin Ahn <sjan137@naver.com>
2021-09-12 19:11:30 +0900

    210912_solved
    153484KB, 1212ms

--- stack_/prob_17298_오큰수.py
+++ stack_/prob_17298_오큰수.py
@@ -1,27 +1,25 @@
 import sys
 
-# 첫 번째 숫자에 대해 [1]부터 찾다가 큰 숫자를 발견하면 스택(st) 저장
-# 발견 못하면 -1 저장
-# 두 번째 숫자부터
-# 스택의 마지막 숫자(st[-1]) > 현재 숫자인 경우 스택의 마지막 숫자 또 저장
-# 아닌 경우 다시 자신 이후부터 숫자 탐색
-# 탐색 후 없으면 -1 저장
-def search(num_list):
-    for num in num_list[1:]:
-        if num > num_list[0]: return num
-    return -1
-
+# 1. 인덱스를 저장하는 스택 생성(초기값 [0])
+# 2. 스택이 남아있고, 현재 선택한 인덱스[i] 숫자가 스택의 [-1]보다 크면,
+#    [-1]의 원소가 [i]보다 크지 않을 때까지 pop하고,
+#    pop한 인덱스의 result 원소는 [i]에 해당하는 숫자로 저장
+# 3. 2번 과정이 다 끝난 후에는 i를 스택에 저장
+# 4. 모든 숫자를 다 체크한 후에 인덱스 스택에 남은 인덱스에 해당하는 result 원소는 모두 -1로 저장
+# 4번 과정은 처음에 result = [-1] * n으로 생성하면 수행할 필요 없음.
 def main():
     N = int(sys.stdin.readline())
     nums = list(map(int, sys.stdin.readline().split()))
-    st = []
+    idxStack = [0]
+    result = [-1] * N
 
-    for i in range(N):
-        if st and st[-1] > nums[i]: st.append(st[-1])
-        elif i == N-1: st.append(-1)
-        else: st.append(search(nums[i:]))
+    for i in range(1, N):
+        while idxStack and nums[i] > nums[idxStack[-1]]:
+            idx = idxStack.pop()
+            result[idx] = nums[i]
+        idxStack.append(i)
     
-    return st
+    return result
 
 if __name__ == "__main__":
-    print(" ".join(map(str, main())))
+    print(*main())
