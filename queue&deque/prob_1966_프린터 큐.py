ba3d6dac4965bf082e95c29ead847b123d29bc46
Seongjin Ahn <sjan137@naver.com>
2021-09-14 01:05:37 +0900

    Update prob_1966_프린터 큐.py
    31780KB, 104ms
    덱을 이용한 풀이. 오히려 더 느려졌다.

--- queue&deque/prob_1966_프린터 큐.py
+++ queue&deque/prob_1966_프린터 큐.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 
 def main():
     T = int(sys.stdin.readline())
@@ -6,16 +7,21 @@ def main():
 
     for _ in range(T):
         N, idx = map(int, sys.stdin.readline().split())
-        nums = list(map(int, sys.stdin.readline().split()))
-        idx_list = list(range(N))
-        cnt = 1
-        max_idx = nums.index(max(nums))
+        nums = deque(map(int, sys.stdin.readline().split()))
+        idx_list = deque(range(N))
+        cnt = 0
+
+        while True:
+            if nums[0] == max(nums):
+                cnt += 1
+                if idx_list[0] == idx: break
+                else:
+                    nums.popleft()
+                    idx_list.popleft()
+            else:
+                nums.append(nums.popleft())
+                idx_list.append(idx_list.popleft())
         
-        while idx != idx_list[max_idx]:
-            nums = nums[max_idx+1:] + nums[:max_idx]
-            idx_list = idx_list[max_idx+1:] + idx_list[:max_idx]
-            max_idx = nums.index(max(nums))
-            cnt += 1
         result.append(cnt)
 
     return result
