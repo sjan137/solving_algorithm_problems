f4459e0c6ceac02738e2a98beb01b8bcaa8869a1
Seongjin Ahn <sjan137@naver.com>
2021-09-17 01:01:01 +0900

    210917_solved
    32100KB, 92ms
    이미 방문한 적이 있는 좌표도 일단 다른 좌표에 의해 계속 여러 번 저장되면서, 불필요한 비교 연산을 더 진행했기 때문에 메모리 초과가 뜬 것 같다.

--- DFS&BFS/prob_2667_단지 번호 붙이기.py
+++ DFS&BFS/prob_2667_단지 번호 붙이기.py
@@ -1,33 +1,35 @@
 import sys
 from collections import deque
 
-def bfs(board, n, position):
-    dr = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+def bfs(board, n, position, visited):
+    dr = [(0, 0), (-1, 0), (1, 0), (0, -1), (0, 1)]
     area = 0
     q = deque([position])
 
     while q:
         x, y = q.popleft()
-        if board[x][y] == '1':
-            board[x][y] = '0'
-            area += 1
         for xx, yy in dr:
-            if 0 <= x+xx < n and 0 <= y+yy < n and board[x+xx][y+yy] == '1': q.append([x+xx, y+yy])
+            if 0 <= x+xx < n and 0 <= y+yy < n and board[x+xx][y+yy] and not visited[x+xx][y+yy]:
+                q.append([x+xx, y+yy])
+                visited[x+xx][y+yy] = True
+                area += 1
     
     return area
 
 def main():
     N = int(sys.stdin.readline())
-    board = [list(sys.stdin.readline().strip()) for _ in range(N)]
+    board = [list(map(int, list(sys.stdin.readline().strip()))) for _ in range(N)]
+    visited = [[False] * N for _ in range(N)]
     result = []
+    cnt = 0
     
     for i in range(N):
         for j in range(N):
-            if board[i][j] == '0': continue
-            area = bfs(board, N, [i, j])
-            result.append(area)
+            if not board[i][j] or visited[i][j]: continue
+            result.append(bfs(board, N, [i, j], visited))
+            cnt += 1
 
-    return [len(result)] + sorted(result)
+    return [cnt] + sorted(result)
 
 if __name__ == "__main__":
     for i in main():
