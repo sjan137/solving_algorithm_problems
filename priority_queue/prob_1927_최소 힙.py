84ad8bf632f4c2c0569a36acb85ef7c2c3df9d57
Seongjin Ahn <sjan137@naver.com>
2021-09-19 02:51:48 +0900

    210919_solved
    35124KB, 148ms

---
+++ priority_queue/prob_1927_최소 힙.py
@@ -0,0 +1,13 @@
+import sys, heapq
+
+def main():
+    N = int(sys.stdin.readline())
+    hq = []
+    for _ in range(N):
+        command = int(sys.stdin.readline())
+        if command: heapq.heappush(hq, command)
+        else: print(heapq.heappop(hq)) if hq else print(0)
+    return
+
+if __name__ == "__main__":
+    main()
