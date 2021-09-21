7026742e54df13471b7d870c368a82153d7baec6
Seongjin Ahn <sjan137@naver.com>
2021-09-21 17:46:02 +0900

    Update prob_1012_유기농 배추.py
    스택으로 구현한 DFS: 29452KB, 88ms
    BFS: 32124KB, 104ms

--- DFS&BFS/prob_1012_유기농 배추.py
+++ DFS&BFS/prob_1012_유기농 배추.py
@@ -1,6 +1,7 @@
 import sys
 from collections import deque
 
+# BFS 이용 - 32124KB, 104ms
 def bfs(board, visited, n, m, position):
     q = deque([position])
     dr = [(-1, 0), (1, 0), (0, -1), (0, 1)]
@@ -13,6 +14,21 @@ def bfs(board, visited, n, m, position):
             if 0 <= i < n and 0 <= j < m and board[i][j] and not visited[i][j]:
                 visited[i][j] = True
                 q.append([i, j])
+
+# DFS 이용(스택으로 구현. 재귀로 구현하면 재귀 제한 오류 발생) - 29452KB, 88ms
+def dfsByStack(board, visited, n, m, position):
+    stack = [position]
+    dr = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    while stack:
+        cur_i, cur_j = stack[-1]
+        visited[cur_i][cur_j] = True
+        for di, dj in dr:
+            i, j = cur_i+di, cur_j+dj
+            if 0 <= i < n and 0 <= j < m and board[i][j] and not visited[i][j]:
+                stack.append((i, j))
+                break
+        else: stack.pop()
 
 T = int(sys.stdin.readline())
 for _ in range(T):
@@ -29,6 +45,7 @@ for _ in range(T):
         for j in range(M):
             if not ones[i][j] or visited[i][j]: continue
             cnt += 1
-            bfs(ones, visited, N, M, [i, j])
+            # bfs(ones, visited, N, M, (i, j))  # BFS 이용
+            dfsByStack(ones, visited, N, M, (i, j))  # 스택으로 구현한 DFS 이용
     
     print(cnt)
