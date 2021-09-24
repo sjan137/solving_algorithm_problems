b96f7dc23a6fcbe6486740736c2d14d64c5d14fe
Seongjin Ahn <sjan137@naver.com>
2021-09-24 11:55:25 +0900

    210924_solved
    29200KB, 72ms

--- number_theory&combinatorics/prob_05_검문.py
+++ number_theory&combinatorics/prob_05_검문.py
@@ -1,38 +1,25 @@
 import sys
 
-
 def gcd(a, b):
     return gcd(b%a, a) if b%a else a
 
-def solve(n, n_list):
-    gcd_list = []
-    n_list.sort()
-    
-    # 정렬된 리스트의 숫자에서 각각 차이를 구하고, 그 차이들의 최대공약수를 저장
-    if n == 2: gcd_list.append(n_list[1]-n_list[0])
-    else:
-        for i in range(1, n-1):
-            temp = gcd(n_list[i+1]-n_list[i], n_list[i]-n_list[i-1])
-            if temp not in gcd_list: gcd_list.append(temp)
-
-    # 최대공약수 사이의 최대공약수를 구해서 가장 공통적인 최대공약수 구하기
-    gcd_num = gcd_list[0]
-    for i in range(1, len(gcd_list)):
-        gcd_num = gcd(gcd_num, gcd_list[i])
-
-    # result = [gcd_num]
-    # for i in range(2, int(gcd_num**0.5)+1):
-    #     if gcd_num % i == 0: 
-    #         result.append(i)
-    #         result.append(gcd_num//i)
-    # result = list(set(result))
-    # print(*result)
+def getDiv(n):
     result = []
-    for i in range(2, gcd_num+1):
-        if gcd_num % i == 0: result.append(i)
-    print(*result)
-    return None
+    for i in range(1, int(n**0.5)+1):
+        if n % i: continue
+        result.append(i)
+        if n // i != i: result.append(int(n//i))
+    return sorted(result[1:])
+
+def solve(n, n_list):
+    # 정렬된 리스트의 숫자에서 각각 차이를 구하고, 그 차이들의 최대공약수를 저장
+    n_list.sort()
+    diff_list = list(set([n_list[i]-n_list[i-1] for i in range(1, n)]))
+    gcd_num = diff_list[0]
+    for i in range(1, len(diff_list)):
+        gcd_num = min(gcd_num, gcd(diff_list[i], diff_list[i-1]))
+    return getDiv(gcd_num)
 
 N = int(sys.stdin.readline())
 numbers = [int(sys.stdin.readline()) for _ in range(N)]
-solve(N, numbers)
+print(*solve(N, numbers))
