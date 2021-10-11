8585ef7fbc7c3d453e8d7675fc86046021a52ed8
Seongjin Ahn <sjan137@naver.com>
2021-10-11 18:09:24 +0900

    Update prob_1167_트리의 지름.py
    시간 초과

--- tree/prob_1167_트리의 지름.py
+++ tree/prob_1167_트리의 지름.py
@@ -1,6 +1,18 @@
 import sys
 from collections import deque
 
+# 1. 연결된 노드의 개수가 1이라면(끝 노드), 탐색 시작
+# 2. DFS 탐색으로 최대 깊이 구하기
+def DFS(tree_info, checked, start_node):
+    max_depth = 0
+    next_nodes = tree_info[start_node]
+    for next_node, dist in next_nodes:
+        if checked[next_node-1]: continue
+        checked[next_node-1] = True
+        max_depth = max(max_depth, dist+DFS(tree_info, checked, next_node))
+        checked[next_node-1] = False
+    return max_depth
+
 V = int(sys.stdin.readline())
 tree = [[] for _ in range(V+1)]
 for _ in range(V):
@@ -10,5 +22,13 @@ for _ in range(V):
         node = nodes.popleft()
         distance = nodes.popleft()
         tree[idx].append((node, distance))
+checked = [False] * V
+result = 0
 
-print(tree)
+for i in range(1, V+1):
+    if len(tree[i]) != 1: continue
+    checked[i-1] = True
+    result = max(result, DFS(tree, checked, i))
+    checked[i-1] = False
+
+print(result)
