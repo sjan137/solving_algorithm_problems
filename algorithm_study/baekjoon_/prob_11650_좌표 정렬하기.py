5a568d661a609fec44b9f61111cc90e9907fc4af
Seongjin Ahn <sjan137@naver.com>
2021-11-06 11:23:06 +0900

    211106_solved
    57672KB, 456ms

--- algorithm_study/baekjoon_/prob_11650_좌표 정렬하기.py
+++ algorithm_study/baekjoon_/prob_11650_좌표 정렬하기.py
@@ -3,5 +3,5 @@ import sys
 N = int(sys.stdin.readline())
 locations = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
-for xy in sorted(locations):
+for xy in sorted(locations, key=lambda x: (x[1], x[0])):
     print(*xy)
