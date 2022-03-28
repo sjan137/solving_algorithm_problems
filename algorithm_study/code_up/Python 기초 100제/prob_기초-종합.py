804d5d159a6d2f7562bd519ff728b72363aade88
seongjin <sjan137@naver.com>
2022-03-28 15:35:18 +0900

    CodeUp 파이썬 기초 100제 중 84~90번 풀이

--- algorithm_study/code_up/Python 기초 100제/prob_기초-종합.py
+++ algorithm_study/code_up/Python 기초 100제/prob_기초-종합.py
@@ -61,3 +61,58 @@ for i in range(r):
         for k in range(b):
             print(i, j, k)
 print(r*g*b)
+
+#---------- 6084 소리 파일 저장용량 계산하기 ----------
+import sys
+
+h, b, c, s = map(int, sys.stdin.readline().split())
+print(f"{h*b*c*s/(8*1024*1024):.1f} MB")
+
+#---------- 6085 그림 파일 저장용량 계산하기 ----------
+import sys
+
+w, h, b = map(int, sys.stdin.readline().split())
+print(f"{w*h*b/(8*1024*1024):.2f} MB")
+
+#---------- 6086 거기까지! 이제 그만~ ----------
+import sys
+
+n = int(sys.stdin.readline())
+s, result = 1, 0
+while 1:
+    result += s
+    s += 1
+    if result >= n:
+        break
+
+print(result)
+
+#---------- 6087 3의 배수는 통과 ----------
+import sys
+
+n = int(sys.stdin.readline())
+for i in range(1, n+1):
+    if i%3 == 0: continue
+    print(i, end=' ')
+
+#---------- 6088 수 나열하기1 ----------
+import sys
+
+a, d, n = map(int, sys.stdin.readline().split())
+print(a+d*(n-1))
+
+#---------- 6089 수 나열하기2 ----------
+import sys
+
+a, r, n = map(int, sys.stdin.readline().split())
+print(a*(r**(n-1)))
+
+#---------- 6090 수 나열하기3 ----------
+import sys
+
+a, m, d, n = map(int, sys.stdin.readline().split())
+print(a*(m**(n-1))+d*sum([m**i for i in range(n-1)]))
+# a
+# a*m+d=am+d
+# (a*m+d)*m+d=amm+d(m+1)
+# ammm+dm(m+1)+d=ammm+d(mm+m+1)
